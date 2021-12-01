537d86caa79434df661eafb35a1c7d3371930af4
Darko Vrbaski <vrbaski.ra200.2018@uns.ac.rs>
2021-12-01 02:01:37 +0100

    project initiation

---
+++ isa-project/src/test/java/isaproject/IsaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package isaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

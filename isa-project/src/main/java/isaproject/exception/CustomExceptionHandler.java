9dbfe179ba44f6148d8a7f73793c4e64b601410c
Darko Vrbaski <vrbaski.ra200.2018@uns.ac.rs>
2022-04-08 23:05:13 +0200

    feat: custom exception handler

---
+++ isa-project/src/main/java/isaproject/exception/CustomExceptionHandler.java
@@ -0,0 +1,15 @@
+package isaproject.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+class CustomExceptionHandler extends BaseExceptionHandler {
+
+	public CustomExceptionHandler() {
+        registerMapping(UsernameNotFoundException.class, HttpStatus.NOT_FOUND);
+        registerMapping(DisabledException.class, HttpStatus.UNAUTHORIZED);
+	}
+}
